212d2a238163aee3d67025d4fa6077335a7cd207
keep_chr <dobrozhan.a@gmail.com>
2020-02-23 01:29:49 +0200

    init: upload task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  
+  if (typeof matrix == 'undefined' || matrix == []) {
+    return []
+  } else {  
+    
+    let sortedArr = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+      if ( i % 2 == 0) {
+        for (let j = 0; j < matrix[i].length; j++) {
+          sortedArr.push(matrix[i][j]); 
+        }
+      } else if ( i % 2 == 1) {
+        let reversedArr = matrix[i].reverse();
+        for (let n = 0; n < reversedArr.length; n++) {
+          sortedArr.push(reversedArr[n]); 
+        }
+      }
+    }
+    return sortedArr;
+  }
 }
